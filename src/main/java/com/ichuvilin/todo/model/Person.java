661624373e43fb2819f4ce553e780fa15ba0273d
Ivan Chuvilin <i_chuvilin@outlook.com>
2023-02-10 21:06:43 +0700

    create person and tasks model

---
+++ src/main/java/com/ichuvilin/todo/model/Person.java
@@ -0,0 +1,30 @@
+package com.ichuvilin.todo.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "person")
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class Person {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "username")
+    private String username;
+
+    @Column(name = "password")
+    private String password;
+
+    @OneToMany(mappedBy = "owner")
+    private List<Tasks> tasks;
+
+}
